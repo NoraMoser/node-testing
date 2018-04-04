78bbf56a6fd50808e1bec34c17b8897cfa6870f7
Moser <noralmoser@gmail.com>
2018-04-04 15:42:11 -0500

    more testing

--- server/server.js
+++ server/server.js
@@ -3,7 +3,21 @@ const express = require('express');
 var app = express();
 
 app.get('/', (reqq, res) => {
-    res.send('Hello world!');
+    res.status(404).send({
+        error: 'Page not found',
+        name: 'Todo App'
+    });
 });
 
+app.get('/users', (req, res) => {
+    res.send([
+        {name: 'Nora', age: 32},
+        {name: 'Ashley', age: 33},
+        {name: 'Arthur', age: 3},
+        {name: 'Lachlan', age: 1}
+    ])
+})
+
 app.listen(3000);
+
+module.exports.app = app;
